5382373d34fe8a0df1a49fe932c754f7a7b21e94
HannoSpreeuw <h.spreeuw@esciencecenter.nl>
2016-03-23 17:54:52 +0100

    Quadratic differences computed on a GPU, but possibly not correct yet

--- hello-gpu.py
+++ hello-gpu.py
@@ -1,27 +1,66 @@
 import pycuda.autoinit
 import pycuda.driver as drv
-import numpy
+import numpy as np
+import time
 
 from pycuda.compiler import SourceModule
+
+
 mod = SourceModule("""
-__global__ void multiply_them(float *dest, float *a, float *b)
+__global__ void quadratic_difference(int *dest, int N, float *x, float *y, float *z, float *ct)
 {
-  const int i = threadIdx.x;
-  dest[i] = a[i] * b[i];
+    // const int i = threadIdx.x;
+    // const int j = threadIdx.y;
+    // const int N = 1500;
+
+    unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+    unsigned int j = blockIdx.y * blockDim.y + threadIdx.y;
+    unsigned int pos = i * N + j;
+
+    // if(i >= N || j >= N) return;
+
+    dest[pos] = powf(x[i] - x[j], 2) + powf(y[i] - y[j], 2) + powf(z[i] - z[j], 2) - powf(ct[i] - ct[j], 2) ;
 }
 """)
 
-multiply_them = mod.get_function("multiply_them")
+quadratic_difference= mod.get_function("quadratic_difference")
 
-N = 1600
+N = 1500
 
-a = numpy.random.randn(N).astype(numpy.float32)
-b = numpy.random.randn(N).astype(numpy.float32)
+x = np.random.randn(N).astype(np.float32)
+y = np.random.randn(N).astype(np.float32)
+z = np.random.randn(N).astype(np.float32)
+ct = np.random.randn(N).astype(np.float32)
 
-dest = numpy.zeros_like(a)
-multiply_them(
-        drv.Out(dest), drv.In(a), drv.In(b),
-        block=(400,1,1), grid=(1,1))
+dest = np.identity(N, 'int')
 
-print(dest-a*b)
+block_size = 1024
+block_size_x = int(np.sqrt(block_size))
+block_size_y = int(np.sqrt(block_size))
+
+# We need N threads for i and N threads for j in quadratic_difference.
+problem_size = int(np.ceil(N * (N - 1)/2))
+
+gridx = int(np.ceil(problem_size/block_size_x))
+gridy = int(np.ceil(problem_size/block_size_y))
+
+# create two timers so we can speed-test each approach
+start = drv.Event()
+end = drv.Event()
+
+start.record() # start timing
+
+quadratic_difference(
+        drv.Out(dest), np.int32(N), drv.In(x), drv.In(y), drv.In(z), drv.In(ct), 
+        block=(block_size_x, block_size_y, 1), grid=(gridx, gridy))
+
+end.record() # end timing
+# calculate the run length
+end.synchronize()
+secs = start.time_till(end)*1e-3
+
+print()
+print('Times taken is {0:.2e}s.'.format(secs))
+print()
+print(dest)
 
