c6fcafbee6eb03262f80d640fee33654e7c1e826
HannoSpreeuw <h.spreeuw@esciencecenter.nl>
2016-03-25 17:01:41 +0100

    Commented out the code that was used for checking the results

--- correlations.py
+++ correlations.py
@@ -43,7 +43,7 @@ __global__ void quadratic_difference(int *correlations, int N, float *x, float *
 
 quadratic_difference= mod.get_function("quadratic_difference")
 
-N = 4500
+N = 1500
 
 x = np.random.randn(N).astype(np.float32)
 y = np.random.randn(N).astype(np.float32)
@@ -55,10 +55,17 @@ y_gpu = drv.mem_alloc(y.nbytes)
 z_gpu = drv.mem_alloc(z.nbytes)
 ct_gpu = drv.mem_alloc(ct.nbytes)
 
+start_transfer = time.time()
+
 drv.memcpy_htod(x_gpu, x)
 drv.memcpy_htod(y_gpu, y)
 drv.memcpy_htod(z_gpu, z)
 drv.memcpy_htod(ct_gpu, ct)
+
+end_transfer = time.time()
+
+print()
+print('Data transfer from host to device took {0:.2e}s.'.format(end_transfer -start_transfer))
 
 correlations = np.empty((N, N), np.int32)
 correlations_gpu = drv.mem_alloc(correlations.nbytes)
@@ -87,38 +94,45 @@ end.record() # end timing
 end.synchronize()
 secs = start.time_till(end)*1e-3
 
+start_transfer = time.time()
+
 drv.memcpy_dtoh(correlations, correlations_gpu)
+
+end_transfer = time.time()
+
+print()
+print('Data transfer from device to host took {0:.2e}s.'.format(end_transfer -start_transfer))
 
 print()
 print('Times taken is {0:.2e}s.'.format(secs))
 print()
-correlations = correlations.reshape(N, N)
-print('correlations = ', correlations)
-print()
-print('correlations.max() = {0}, correlations.argmax() = {1}'.format(correlations.max(), np.unravel_index(correlations.argmax(), correlations.shape)))
-# check for symmetry.
-print()
-print('Symmetry check, this should be zero: {0}'.format(np.sum(np.abs(correlations - correlations.T))))
-
-check = np.identity(correlations.shape[0], correlations.dtype)
-
-# Checkif output is correct.
-for i in range(check.shape[0]):
-    for j in range(i+1, check.shape[1]):
-        if (ct[i]-ct[j])**2 < (x[i]-x[j])**2  + (y[i] - y[j])**2 + (z[i] - z[j])**2:
-          check[i, j] = 1
-          check[j, i] = check[i, j]
-
-print()
-print()
-print('check = ', check)
-print()
-print('check.max() = {0}'.format(check.max()))
-print()
-print('Symmetry check, this should be zero: {0}'.format(np.sum(np.abs(check - check.T))))
-print()
-print()
-print()
-print('This should be close to zero: {0}'.format(np.max(np.abs(check - correlations))))
-print()
-print('check - correlations = ', check -correlations)
+#   correlations = correlations.reshape(N, N)
+#   print('correlations = ', correlations)
+#   print()
+#   print('correlations.max() = {0}, correlations.argmax() = {1}'.format(correlations.max(), np.unravel_index(correlations.argmax(), correlations.shape)))
+#   # check for symmetry.
+#   print()
+#   print('Symmetry check, this should be zero: {0}'.format(np.sum(np.abs(correlations - correlations.T))))
+#   
+#   check = np.identity(correlations.shape[0], correlations.dtype)
+#   
+#   # Checkif output is correct.
+#   for i in range(check.shape[0]):
+#       for j in range(i+1, check.shape[1]):
+#           if (ct[i]-ct[j])**2 < (x[i]-x[j])**2  + (y[i] - y[j])**2 + (z[i] - z[j])**2:
+#             check[i, j] = 1
+#             check[j, i] = check[i, j]
+#   
+#   print()
+#   print()
+#   print('check = ', check)
+#   print()
+#   print('check.max() = {0}'.format(check.max()))
+#   print()
+#   print('Symmetry check, this should be zero: {0}'.format(np.sum(np.abs(check - check.T))))
+#   print()
+#   print()
+#   print()
+#   print('This should be close to zero: {0}'.format(np.max(np.abs(check - correlations))))
+#   print()
+#   print('check - correlations = ', check -correlations)
