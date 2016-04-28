1ed1724ad197e2e6ecad716ec9a39030d7a386e4
HannoSpreeuw <h.spreeuw@esciencecenter.nl>
2016-04-28 09:49:00 +0200

    Tried to improve timing accuracy using events, but cuda.event_elapsed_time(start_computations, end_computations) gives CUDA_ERROR_INVALID_HANDLE

--- correlations-Numba.py
+++ correlations-Numba.py
@@ -6,22 +6,22 @@ import time
 def quadratic_difference(correlations, x, y, z, ct):
 
     i, j = cuda.grid(2)
- 
+
     n = correlations.shape[0]
     m = correlations.shape[1]
 
-    if i < n and j < m:
-        diffct = ct[i] - ct[j]
-        diffx  = x[i] - x[j]
-        diffy  = y[i] - y[j]
-        diffz  = z[i] - z[j]
+    l = i + j - int(m/2)
+ 
+    if i < n and j < m and l > 0 and l < n:
+        diffct = ct[l] - ct[l]
+        diffx  = x[l] - x[l]
+        diffy  = y[l] - y[l]
+        diffz  = z[l] - z[l]
         if diffct * diffct < diffx * diffx + diffy * diffy + diffz * diffz:
             correlations[i, j] = 1
-        else:
-            correlations[i, j] = 0
 
 def main():
-    N = 3000000
+    N = 30000
 
     x = np.random.random(N).astype(np.float32)
     y = np.random.random(N).astype(np.float32)
@@ -46,7 +46,7 @@ def main():
     sliding_window_width = 2 * N_light_crossing
     # problem_size = N * sliding_window_width
 
-    correlations = np.empty((N, sliding_window_width), 'b')
+    correlations = np.zeros((N, sliding_window_width), 'b')
     print()
     print("Number of bytes needed for the correlation matrix = {0:.3e} ".format(correlations.nbytes))
 
@@ -59,14 +59,20 @@ def main():
     gridx = int(np.ceil(correlations.shape[0]/block_size_x))
     gridy = int(np.ceil(correlations.shape[1]/block_size_y))
 
-    start_computations = time.time()
+    start_computations = cuda.event(timing = True)
+
+    start_c = time.time()
 
     quadratic_difference[(gridx, gridy), (block_size_x, block_size_y)](correlations_gpu, x_gpu, y_gpu, z_gpu, ct_gpu)
 
-    end_computations = time.time()
+    end_computations = cuda.event(timing = True)
+
+    end_computations.synchronize()
+
+    end_c = time.time()
 
     print()
-    print('Time taken for computations is {0:.2e}s.'.format(end_computations - start_computations))
+    print('Time taken for computations is {0:.3e}s.'.format(end_c - start_c))
 
     start_transfer = time.time()
 
@@ -81,19 +87,16 @@ def main():
    
     # Checkif output is correct.
     for i in range(check.shape[0]):
-        for j in range(check.shape[1]):
-            if (ct[i]-ct[j])**2 < (x[i]-x[j])**2  + (y[i] - y[j])**2 + (z[i] - z[j])**2:
-                check[i, j] = 1
-    
+        for j in range(i - int(check.shape[1]/2), i + int(check.shape[1]/2)):
+            if (j < check.shape[0]) and (j >= 0):
+                if (ct[i]-ct[j])**2 < (x[i]-x[j])**2  + (y[i] - y[j])**2 + (z[i] - z[j])**2:
+                    check[i, j - i +  int(check.shape[1]/2)] = 1
+
     print()
     print()
     print('check = ', check)
     print()
     print('check.max() = {0}'.format(check.max()))
-    print()
-    print('Symmetry check, this should be zero: {0}'.format(np.sum(np.abs(check - check.T))))
-    print()
-    print()
     print()
     print('This should be close to zero: {0}'.format(np.max(np.abs(check - correlations))))
     print()
