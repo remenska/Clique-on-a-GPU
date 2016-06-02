5dc18c18272d40240e6009265ba52d742e6a7a94
daniela <remenska@gmail.com>
2016-06-02 16:03:29 +0200

    damn finally

--- correlations-Numba.py
+++ correlations-Numba.py
@@ -18,7 +18,7 @@ def quadratic_difference(correlations, x, y, z, ct):
     bwy = cuda.blockDim.y
 
     
-    i, j = cuda.grid(2) # global position of the thread
+    i, jj = cuda.grid(2) # global position of the thread
 
     n, m = correlations.shape  # n = N, m = window size
 
@@ -40,35 +40,36 @@ def quadratic_difference(correlations, x, y, z, ct):
         base_hits[3, tx] = ct[i]
 
 
-    # surrounding_hits = cuda.shared.array((4, block_size_y), dtype=f4)
+    surrounding_hits = cuda.shared.array((4, block_size_y), dtype=f4)
 
-    #if tx == 0 and l >= 0 and l < n:
+    if jj == ty + by*bwy and tx == 0 and jj <m:
     # if tx ==0 and j < m
-    #    surrounding_hits[0, ty] = x[j]
-    #    surrounding_hits[1, ty] = y[j]
-    #    surrounding_hits[2, ty] = z[j]
-    #    surrounding_hits[3, ty] = ct[j]
+        surrounding_hits[0, ty] = x[jj]
+        surrounding_hits[1, ty] = y[jj]
+        surrounding_hits[2, ty] = z[jj]
+        surrounding_hits[3, ty] = ct[jj]
 
     cuda.syncthreads()
-    print(base_hits)
-
+    #if tx == 2 and bx == 0 and ty == 3 and by == 1:
+    #    from pdb import set_trace
+    #    set_trace()
     #if i < n and j < m and l >= 0 and l < n:
     #if i < n and j < m:
-    if i == ( tx + bx * bwx ) and j == ( ty + by * bwy ) and  i < n and j < m:
+    if i == ( tx + bx * bwx ) and jj == ( ty + by * bwy ) and  i < n and jj < m and jj>i:
         diffx  = base_hits[0, tx] - base_hits[0, ty]
         diffy  = base_hits[1, tx] - base_hits[1, ty]
         diffz  = base_hits[2, tx] - base_hits[2, ty]
         diffct = base_hits[3, tx] - base_hits[3, ty]
 
         if diffct * diffct <= diffx * diffx + diffy * diffy + diffz * diffz:
-            if j>=i:
-                correlations[i, j] = 1
+            if jj>i:
+                correlations[i, jj] = 1
 
 def main():
-    start_computations = cuda.event(timing = True)
-    end_computations   = cuda.event(timing = True)
+    #start_computations = cuda.event(timing = True)
+    #end_computations   = cuda.event(timing = True)
 
-    N = 64
+    N = 4096
 
     x = np.random.random(N).astype(np.float32)
     y = np.random.random(N).astype(np.float32)
@@ -78,22 +79,22 @@ def main():
     # pickle the data for reuse
     np.save("./x.pkl", x)
     np.save("./y.pkl", y)
-    np.save("./z.pkl", z)
+    np.save("./z.pkl", z)       
     np.save("./ct.pkl", ct)
-    start_transfer = time.time()
+    #start_transfer = time.time()
 
     x_gpu = cuda.to_device(x)
     y_gpu = cuda.to_device(y)
     z_gpu = cuda.to_device(z)
     ct_gpu = cuda.to_device(ct)
 
-    end_transfer = time.time()
+    #end_transfer = time.time()
 
     print()
-    print('Data transfer from host to device plus memory allocation on device took {0:.2e}s.'.format(end_transfer - start_transfer))
+    #print('Data transfer from host to device plus memory allocation on device took {0:.2e}s.'.format(end_transfer - start_transfer))
 
     # The number of consecutive hits corresponding to the light crossing time of the detector (1km/c).
-    N_light_crossing = 64
+    N_light_crossing = 1024
 
     # This used to be 2 * N_light_crossing, but caused redundant calculations.
     sliding_window_width =  N_light_crossing
@@ -108,25 +109,25 @@ def main():
     gridx = int(np.ceil(correlations.shape[0]/block_size_x))
     gridy = int(np.ceil(correlations.shape[1]/block_size_y))
 
-    start_computations.record()
+    #start_computations.record()
 
     quadratic_difference[(gridx, gridy), (block_size_x, block_size_y)](correlations_gpu, x_gpu, y_gpu, z_gpu, ct_gpu)
 
-    end_computations.record()
+    #end_computations.record()
 
-    end_computations.synchronize()
+    ##end_computations.synchronize()
 
     print()
-    print('Time taken for computations is {0:.3e}s.'.format(1e-3 * start_computations.elapsed_time(end_computations)))
+    #print('Time taken for computations is {0:.3e}s.'.format(1e-3 * start_computations.elapsed_time(end_computations)))
 
-    start_transfer = time.time()
+    #start_transfer = time.time()
 
     correlations_gpu.copy_to_host(correlations)
 
-    end_transfer = time.time()
+    #end_transfer = time.time()
 
     print()
-    print('Data transfer from device to host took {0:.2e}s.'.format(end_transfer -start_transfer))
+    #print('Data transfer from device to host took {0:.2e}s.'.format(end_transfer -start_transfer))
 
     print()
     print('correlations = ', correlations)
